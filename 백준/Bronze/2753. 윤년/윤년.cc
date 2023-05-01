f65468d7d7bc4524418465d308288e9b168779b8
Changmok Lee <mokcandy97@gmail.com>
2023-04-30 21:09:17 -0600

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int year;
+
+	cin >> year;
+
+	if ((year % 400) == 0) {
+		cout << 1 << '\n';
+	}
+	else if ((year % 4 == 0) && (year % 100 != 0)) {
+		cout << 1 << '\n';
+	}
+	else {
+		cout << 0 << '\n';
+	}
+
+	return 0;
+}
