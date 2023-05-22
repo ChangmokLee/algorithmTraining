57f40979d747ff5f7c4ca78784b679d0d950d426
Changmok Lee <mokcandy97@gmail.com>
2023-05-22 04:12:46 -0600

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.cc
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -1,12 +1,13 @@
 #include <iostream>
+
 using namespace std;
 
 int main() {
 	double a, b;
+
 	cin >> a >> b;
 
-	cout.precision(12);
-	cout << fixed;
-	cout << a / b;
+	cout.precision(16);
+	cout << a / b << "\n";
 	return 0;
 }
