425ba3628e703144f9d962b6ce9c3209d5bfb888
Changmok Lee <mokcandy97@gmail.com>
2023-08-15 22:41:21 -0600

    [Bronze V] Title: A-B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1001. A－B/A－B.cc
+++ 백준/Bronze/1001. A－B/A－B.cc
@@ -1,10 +1,12 @@
 #include <iostream>
+
 using namespace std;
 
 int main() {
 	int a, b;
-	cin >> a;
-	cin >> b;
-	cout << a - b << '\n';
+
+	cin >> a >> b;
+
+	cout << a - b << "\n";
 	return 0;
 }
