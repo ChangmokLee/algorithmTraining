81d02a31a0c1fae2883114c1bf1a7649feb25b16
Changmok Lee <mokcandy97@gmail.com>
2023-08-20 19:39:11 -0600

    [Bronze III] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.cc
+++ 백준/Bronze/2588. 곱셈/곱셈.cc
@@ -4,13 +4,11 @@ using namespace std;
 
 int main() {
 	int a, b;
-
 	cin >> a >> b;
 
-	cout << a * (b % 10) << '\n';
-	cout << a * ((b % 100) / 10) << '\n';
-	cout << a * (b / 100) << '\n';
-	cout << a * b << '\n';
-	
+	cout << a * (b % 10) << "\n";
+	cout << a * ((b % 100)/10) << "\n";
+	cout << a * (b / 100) << "\n";
+	cout << a * b << "\n";
 	return 0;
 }
