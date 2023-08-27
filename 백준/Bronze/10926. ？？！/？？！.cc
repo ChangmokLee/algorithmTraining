2b0c737cf039832d13b2dfa1415f40ed1314c911
Changmok Lee <mokcandy97@gmail.com>
2023-08-26 21:31:13 -0600

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10926. ？？！/？？！.cc
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -1,10 +1,12 @@
 #include <iostream>
+
 using namespace std;
 
 int main() {
-	string name;
-	cin >> name;
+	string id;
 
-	cout << name + "??!" << '\n';
+	cin >> id;
+
+	cout << id + "??!" << "\n";
 	return 0;
 }
